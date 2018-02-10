881fcf4b5fc432e58ec8b476bdfaf19363fac5da
Lucas <lucaskesselman@gmail.com>
2018-02-09 20:07:16 -0600

    hh

--- status.js
+++ status.js
@@ -1,10 +1,26 @@
 
-
+var Particle = require('particle-api-js');
 var particle = new Particle();
+var token;
 
+particle.login({username: 'lucaskesselman@gmail', password: 'lucas1289'}).then(
+  function(data) {
+    token = data.body.access_token;
+  },
+  function (err) {
+    console.log('Could not log in.', err);
+  }
+);
 
 particle.getEventStream({ auth: token}).then(function(stream) {
-  stream.on('event', function(data) {
-    console.log("Event: ", data);
+  stream.on('Wifi Strength', function(data) {
+    
+
+    console.log("Wifi Strength: ", data);
+
+    document.getElementByID('wifiID') = data;
+
+
   });
 });
+
